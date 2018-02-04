1490e5efedc22c47092774ae6cc6969d27bd3117
luo0412 <1095847440@qq.com>
2018-02-04 23:09:09 +0800

    add demo

---
+++ layout-parcel-react/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class extends React.Component {
+    render() {
+        return (
+            <div>正在加载</div>
+        )
+    }
+}
--- main.js
+++
@@ -1,5 +0,0 @@
-import classes from './main.css'
-
-export default ()=>{
-    console.log(classes);
-}
