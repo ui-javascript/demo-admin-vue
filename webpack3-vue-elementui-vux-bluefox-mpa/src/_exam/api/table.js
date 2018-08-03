2e6a3a03a73aeff9d15af2e640d6896514270afb
luo0412 <1095847440@qq.com>
2018-08-03 10:34:31 +0800

    small

--- webpack3-vue-elementui-vux-bluefox-mpa/src/_exam/api/table.js
+++
@@ -1,9 +0,0 @@
-import request from './request'
-
-export function getList(params) {
-    return request({
-        url: '/table/list',
-        method: 'get',
-        params
-    })
-}
--- webpack3-vue-elementui-vux-bluefox-mpa/src/_vux/ajax.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import App from './views/ajaxApp'
-
-
-new Vue({
-    render: h => h(App)
-}).$mount('#app')
