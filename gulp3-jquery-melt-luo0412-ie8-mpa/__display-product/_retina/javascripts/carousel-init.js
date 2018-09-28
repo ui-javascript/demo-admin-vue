69993fe6cbdcc40e18beeccb9ea02b3ea802d22d
luo0412 <1095847440@qq.com>
2018-09-28 13:43:32 +0800

    [gulp-melt]adjust

---
+++ gulp3-jquery-melt-luo0412-ie8-mpa/__display-product/_retina/javascripts/carousel-init.js
@@ -0,0 +1,120 @@
+// MAIN.JS
+//--------------------------------------------------------------------------------------------------------------------------------
+//This is main JS file that contains custom JS scipts and initialization used in this template*/
+// -------------------------------------------------------------------------------------------------------------------------------
+// Author: Designova.
+// Website: http://www.designova.net 
+// Copyright: (C) 2014 
+// -------------------------------------------------------------------------------------------------------------------------------
+
+/*global $:false */
+/*global window: false */
+
+(function(){
+  "use strict";
+
+
+$(function ($) {
+
+    
+
+
+     
+   $("#about-carousel").owlCarousel({
+        navigation : true,
+        pagination: false,
+        responsive: true,
+        items: 1,
+        touchDrag: true,
+        navigationText: false,
+        mouseDrag: true,
+        itemsDesktop: [3000,1],
+        itemsDesktopSmall: [1440,1],
+        itemsTablet:[1024,1],
+        itemsTabletSmall: [600,1],
+        itemsMobile: [360,1],
+        autoPlay: false,
+        transitionStyle : "backSlide",
+        autoHeight: true,
+    });
+     
+    // Custom Navigation Events
+    $(".about-carousel-custom-next-trigger").click(function(){
+   $("#about-carousel").trigger('owl.next');
+    return(false);
+    })
+
+
+    $("#testimonials-carousel").owlCarousel({
+        navigation : false,
+        pagination: true,
+        responsive: true,
+        items: 1,
+        touchDrag: true,
+        navigationText: false,
+        mouseDrag: true,
+        itemsDesktop: [3000,1],
+        itemsDesktopSmall: [1440,1],
+        itemsTablet:[1024,1],
+        itemsTabletSmall: [600,1],
+        itemsMobile: [360,1],
+        autoPlay: false,
+      });
+
+    $("#contact-carousel").owlCarousel({
+        navigation : false,
+        pagination: true,
+        responsive: true,
+        items: 1,
+        touchDrag: true,
+        navigationText: false,
+        mouseDrag: true,
+        itemsDesktop: [3000,1],
+        itemsDesktopSmall: [1440,1],
+        itemsTablet:[1024,1],
+        itemsTabletSmall: [600,1],
+        itemsMobile: [360,1],
+        autoPlay: false,
+        autoHeight: true,
+      });
+
+    // Custom Navigation Events
+    $(".contact-carousel-custom-next-trigger").click(function(){
+   $("#contact-carousel").trigger('owl.next');
+    return(false);
+    })
+
+    $("#stats-carousel").owlCarousel({
+        navigation : false,
+        pagination: true,
+        responsive: true,
+        items: 1,
+        touchDrag: true,
+        navigationText: false,
+        mouseDrag: true,
+        itemsDesktop: [3000,1],
+        itemsDesktopSmall: [1440,1],
+        itemsTablet:[1024,1],
+        itemsTabletSmall: [600,1],
+        itemsMobile: [360,1],
+        autoPlay: false,
+        autoHeight: true,
+      });
+     
+
+
+
+});
+// $(function ($)  : ends
+
+})();
+//  JSHint wrapper $(function ($)  : ends
+
+
+
+
+
+
+
+  
+
