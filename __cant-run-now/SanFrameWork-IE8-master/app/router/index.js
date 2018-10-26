fe0cd73acb69af78906ca8ad815ba5f95babc29a
luo0412 <1095847440@qq.com>
2018-10-27 00:30:32 +0800

    [sanjs]调整配置 adjust

---
+++ __cant-run-now/SanFrameWork-IE8-master/app/router/index.js
@@ -0,0 +1,19 @@
+var FullPage = require('../../pages/FullPage.san');
+var Hello = require('../../pages/Hello.san');
+var Hi = require('../../pages/Hi.san');
+
+router.add({
+    rule: '/',
+    Component: Hello,
+    target:'#app',
+});
+router.add({
+    rule: '/a',
+    Component:Hi,
+    target:'#app'
+})
+router.listen(function(e){
+    console.log(e)
+})
+// router.setMode('html5')
+router.start();
