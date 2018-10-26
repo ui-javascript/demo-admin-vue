8d8423f14461be82712d1d9237ec7001c8ec95a8
luo0412 <1095847440@qq.com>
2018-10-26 11:22:18 +0800

    [sanjs]adjsut

---
+++ webpack4-sanjs-jquery-libin1991-ie8-mpa/config/system/config.vue-admin-mall.js
@@ -0,0 +1,15 @@
+// 处理路径
+const  path = require('path')
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+module.exports = {
+    PORT: 9528,
+    IE8: false,
+    PAGES: "_pages-vue-admin",
+    CDN: "/",
+    MODULES: "mall",
+    COPYDIR_IGNORE: "**",
+    RESOLVE_ALIAS: {}
+};
