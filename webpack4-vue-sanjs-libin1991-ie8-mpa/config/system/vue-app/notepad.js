c6d25d9ed622b79657616c60ba194ba3b0a6d4fa
luo0412 <1095847440@qq.com>
2018-10-29 18:27:01 +0800

    [sanjs]添加notepad small

---
+++ webpack4-vue-sanjs-libin1991-ie8-mpa/config/system/vue-app/notepad.js
@@ -0,0 +1,29 @@
+// 处理路径
+const  path = require('path')
+function resolve(dir) {
+    return path.join(__dirname, '../../../' + dir)
+}
+
+module.exports = {
+    system: {
+        supportIE8: false,
+        pages: "_pages-vue-app",
+        modules: "notepad",
+        resolveAlias: {
+            '@': resolve('_page-vue-app/notepad'),
+        },
+        externals: {
+        },
+    },
+    build: {
+        env: {
+            NODE_ENV: '"production"',
+        },
+    },
+    dev: {
+        port: 9527,
+        env: {
+            NODE_ENV: '"development"',
+        },
+    },
+};
