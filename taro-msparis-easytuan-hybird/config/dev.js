ee062b0bcb3e87543d6825d6e2aba57d4c4a25b5
luo0412 <1095847440@qq.com>
2018-11-14 13:24:41 +0800

    adjust

--- taro-msparis-easytuan-hybird/config/dev.js
+++ taro-msparis-easytuan-hybird/config/dev.js
@@ -7,8 +7,9 @@ module.exports = {
   weapp: {},
   h5: {
     devServer: {
-      host: 'localhost', // 如需局域网（如手机）访问，请更换为0.0.0.0
-      // host: '0.0.0.0', // 如需局域网（如手机）访问，请更换为0.0.0.0
+      host: 'localhost',
+      // 如需局域网（如手机）访问，请更换为0.0.0.0
+      // host: '0.0.0.0',
       port: 8088,
       https: false
     }
