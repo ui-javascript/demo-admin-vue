ab5b73666c9f0687486a8209eecde336fdfcc099
luo0412 <1095847440@qq.com>
2018-11-15 19:33:51 +0800

    [bluefox]adjust

--- webpack4-vue-sanjs-libin1991-ie8-mpa/_pages-san/router/router.js
+++ webpack4-vue-sanjs-libin1991-ie8-mpa/_pages-san/router/router.js
@@ -1,30 +1,25 @@
-require("babel-polyfill")
+// require("babel-polyfill")
 require("es5-polyfill")
 
 //IE8 ^4.5.10
-require('es5-shim')
-// require('object-create-ie8')
-// require('object-defineproperty-ie8')
-require('console-polyfill')
+// import 'es5-shim';
+// import 'object-create-ie8';
+import 'object-defineproperty-ie8';
+import 'console-polyfill';
 
 //比IE8的JSON好用
-require('json3');
+// import 'json3';
 //性能超高的Promise实现
-require('bluebird')
-require('fetch-polyfill2')
+// import 'bluebird';
+// import 'fetch-polyfill2';
 
 
-var san = require('san')
-require("./san.scss")
+import san from 'san'
+import "./san.scss"
 
-var router = require('san-router').router
-// import {router} from 'san-router'
-
-var HelloApp = require('./views/HelloApp.san');
-var HelloApp2 = require('./views/HelloApp2.san');
-// import HelloApp from './views/HelloApp.san'
-// import HelloApp2 from './views/HelloApp2.san'
-
+import {router} from 'san-router'
+import HelloApp from './views/HelloApp.san'
+import HelloApp2 from './views/HelloApp2.san'
 
 console.log('hello webpack San')
 
@@ -33,6 +28,7 @@ console.log('hello webpack San')
 router.add({rule: '/', Component: HelloApp, target: '#app'})
 router.add({rule: '/p1', Component: HelloApp, target: '#app'})
 router.add({rule: '/p2', Component: HelloApp2, target: '#app'})
+
 
 // 一定要记得启动
 // router.setMode('html5')
