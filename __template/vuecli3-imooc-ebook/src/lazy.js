da81f6f27ea2f99909a383293a91b09531394023
luo0412 <1095847440@qq.com>
2019-08-09 17:24:26 +0800

    [结构]adjust

--- __template/vuecli3-imooc-ebook/src/lazy.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import lazyload from 'vue-lazyload'
-
-Vue.use(lazyload, {
-  error: require('@/assets/images/loading2.jpeg'),
-  loading: require('@/assets/images/loading2.jpeg')
-})
